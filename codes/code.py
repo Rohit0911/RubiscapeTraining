55e67165f89690e5383c28d7e3521825e2eda1f2
rohitpol0911@gmail.com <rohitpol0911@gmail.com>
2024-07-02 11:38:33 +0530

    first

---
+++ codes/code.py
@@ -0,0 +1,10 @@
+# Hello this is Rohit and Its my second day at office today. I am really excited to work her and learn new things
+
+def fun(n):
+    if n<=1:
+        return n
+    else:
+        return (n-1)+(n-2)
+    
+
+print("The fibonacci value of the number n", )
